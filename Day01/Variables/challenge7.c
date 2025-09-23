abed9b262631f5af078774b8dbd4e83f043eb363
Mohamed Amine <mohamedaminerahmani6@gmail.com>
2025-09-23 10:30:20 +0100

    challenge7

---
+++ Day01/Variables/challenge7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c ;
+    printf("Enter la valeur de a : ");
+    scanf("%d", &a);
+    printf("Enter la valeur de b : ");
+    scanf("%d", &b);
+    printf("Enter la valeur de c : ");
+    scanf("%d", &c);
+    printf("1er nomber : %d\n", a * 2);
+    printf("2er nomber : %d\n", b * 3);
+    printf("3er nomber : %d", c * 5);
+
+    return 0;
+}
